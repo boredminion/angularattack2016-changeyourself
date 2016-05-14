9cc2d41af9f5b35e6dd37deea60ff5a3d06c98e7
kostia777999 <kostya.gorozhanov@mail.ru>
2016-05-14 11:22:15 +0300

    we raised

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+@Component({
+  selector: 'my-about',
+  template: require('./about.component.html'),
+  styles: [require('./about.component.scss')]
+})
+export class AboutComponent implements OnInit {
+  items: FirebaseListObservable<any[]>;
+  constructor(af: AngularFire) {
+    this.items = af.database.list('/item/arr');
+  }
+
+  ngOnInit() {
+    console.log('Hello About');
+  }
+
+}
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { enableProdMode } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { ELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser';
+import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
+
+import {AppComponent} from './app/app.component';
+
+const ENV_PROVIDERS = [];
+// depending on the env mode, enable prod mode or add debugging modules
+if (process.env.ENV === 'build') {
+  enableProdMode();
+} else {
+  ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
+}
+
+bootstrap(AppComponent, [
+    // These are dependencies of our App
+    ...HTTP_PROVIDERS,
+    ...ROUTER_PROVIDERS,
+    ...ENV_PROVIDERS,
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    FIREBASE_PROVIDERS,
+    defaultFirebase('https://changeyouself.firebaseio.com/')
+  ])
+  .catch(err => console.error(err));
