5059a429e483146ef5a22617066cf87068c5e81b
kostia777999 <kostya.gorozhanov@mail.ru>
2016-05-14 19:30:24 +0300

    ui

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators, AbstractControl } from '@angular/common';
 import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods  } from 'angularfire2';
+import { Router } from '@angular/router-deprecated'
 
 @Component({
-    selector: 'login',
+    selector: 'my-login',
     template: require('./login.component.html'),
     styles: [require('./login.component.scss')],
     directives: [FORM_DIRECTIVES]
 })
 export class LoginComponent {
-    form:ControlGroup;
-    email:AbstractControl;
-    password:AbstractControl;
+    form: ControlGroup;
+    email: AbstractControl;
+    password: AbstractControl;
+    massage: string;
 
-    constructor(public af:AngularFire, fb:FormBuilder) {
+    constructor(public af: AngularFire, fb: FormBuilder, public router: Router) {
         this.form = fb.group({
             'email': ['', Validators.required],
             'password': ['', Validators.required]
@@ -24,31 +26,52 @@ export class LoginComponent {
 
     passwordLogin(form) {
         let promise = this.af.auth.login(form);
-        promise.then(response=>{
-            console.log(response)
-        }).catch(err=>{
-            console.log(err)
-        })
+        promise.then(response => {
+            this.router.navigate(['Home']);
+        }).catch(err => {
+            this.massage = "The specified user does not exist.";
+        });
     }
 
     facebookLogin() {
-        this.af.auth.login({
+        let promise = this.af.auth.login({
             provider: AuthProviders.Facebook,
             method: AuthMethods.Popup,
+        });
+        promise.then(response => {
+            this.router.navigate(['Home']);
+            console.log(response);
+        }).catch(err => {
+            console.log(err);
+            this.router.navigate(['Home']);
         });
     }
 
     googleLogin() {
-        this.af.auth.login({
+        let promise = this.af.auth.login({
             provider: AuthProviders.Google,
             method: AuthMethods.Popup,
+        });
+        promise.then(response => {
+            this.router.navigate(['Home']);
+            console.log(response);
+        }).catch(err => {
+            console.log(err);
+            this.router.navigate(['Home']);
         });
     }
 
     twitterLogin() {
-        this.af.auth.login({
+        let promise = this.af.auth.login({
             provider: AuthProviders.Twitter,
             method: AuthMethods.Popup,
+        });
+        promise.then(response => {
+            this.router.navigate(['Home']);
+            console.log(response);
+        }).catch(err => {
+            console.log(err);
+            this.router.navigate(['Home']);
         });
     }
 
