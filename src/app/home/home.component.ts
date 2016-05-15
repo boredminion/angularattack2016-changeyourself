a5550adcad4bc907c1576e499238723454f842f6
denis-andrikevich <tavtant@yandex.ru>
2016-05-15 13:42:58 +0300

    home page autocomplete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router-deprecated'
 
 @Component({
   selector: 'my-home',
@@ -6,13 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styles: [require('./home.component.scss')]
 })
 export class HomeComponent implements OnInit {
+  cities = [];
+  city: any;
+  citiesDropdown = false;
 
-  constructor() {
+  constructor(private http: Http, public router: Router) {
     // Do stuff
+	this.city = {}
+
   }
 
   ngOnInit() {
     console.log('Hello Home');
   }
 
+  getCitites(model){
+	  console.log(model);
+	  if (model.length > 2){
+		  this.http.get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + model).
+			  subscribe(response => {
+				  response.json();
+				  console.log(response.json());
+				  this.cities = response.json().results;
+				  this.citiesDropdown = true;
+			  })
+	  }
+  }
+
+  handleClickCities(cityItem) {
+	  this.city = cityItem;
+	  this.citiesDropdown = false;
+  }
+
+  getCityLocation(){
+	  console.log(this.city.geometry);
+	  this.router.navigate(['Explore', this.city.geometry.location]);
+  }
 }
+
